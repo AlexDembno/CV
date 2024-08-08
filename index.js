9dcc353cf814f152b67cca2b46b9802928039b73
Oleksii Dembnovetskyi <dembnoveckii@gmail.com>
2024-08-08 14:56:24 +0200

    add CV

---
+++ index.js
@@ -0,0 +1,128 @@
+export const translationsDE = {
+  contact: 'Kontaktinformationen',
+  land: 'Stuttgart, Deutschland',
+  technical: 'Technische Fähigkeiten',
+  teamwork: 'Zusammenarbeit',
+  committed: 'Engagiert',
+  punctual: 'Pünktlich',
+  organizational: 'Organisationstalent',
+  willingness: 'Lernbereitschaft',
+  languages: 'Sprachkenntnisse',
+  deutsch: 'Deutsch - B2',
+  english: 'English - B1',
+  ukrainian: 'Ukrainisch - Muttersprache',
+  russian: 'Russisch - Muttersprache',
+  education: 'Bildung',
+  school: 'IT-Schule GoIT',
+  developer: 'Full-Stack-Entwickler, Juni 2022 - Mai 2023',
+  university: 'Nationale Universität „Odessa Maritime Academy“',
+  law: 'LLM, Seerecht, August 2003 - Juni 2009',
+  full: 'Full-Stack-Entwickler',
+  summary: 'Zusammenfassung',
+  text_first:
+    'Ich bin ein selbstmotivierter Full-Stack-Entwickler mit einem Jahr praktischer Erfahrung und einem Jahr akademischer Erfahrung in der Programmierung. Ich habe umfassende Kenntnisse und Fähigkeiten in CSS, HTML, JavaScript, React und Node.js. Meine Programmierreise umfasst den erfolgreichen Abschluss eines Arbeitsprojekts sowie drei Teamprojekte und zwei Einzelprojekte im Rahmen meines Studiums an der renommierten IT-Schule GoIT.',
+  text_second:
+    'Derzeit suche ich nach Möglichkeiten, als Front-End-Entwickler mit Schwerpunkt auf React oder als Full-Stack-Entwickler mit einem Fokus auf JavaScript, React und Node.js zu arbeiten. Ich strebe kontinuierliche Entwicklung an, lerne schnell und genieße das Programmieren. Ich arbeite aktiv an meinem persönlichen Projekt, das es mir ermöglicht, meine Fähigkeiten ständig zu verbessern und neues Wissen in der Praxis anzuwenden.',
+  text_third:
+    'Ich strebe eine weitere Entwicklung im Bereich Programmierung an, mit einem Schwerpunkt auf beruflichem Wachstum, effektiver Kommunikation und produktiver Teamarbeit. Mein Ziel ist es, meine Fähigkeiten kontinuierlich zu verbessern und durch harte Arbeit und das Streben nach Exzellenz hohe Ergebnisse zu erzielen.',
+  experience: 'Arbeitserfahrung',
+  brovko_1: 'Full-Stack-Entwickler, Teilzeit: August 2023 – Mai 2024',
+  brovko_2:
+    'Technologien: HTML, Sass, JavaScript, Webpak, React, Redux Toolkit, Node.js, MongoDB, Express, Git, Docker, Swagger, Trello.',
+  brovko_3:
+    'Verantwortlichkeiten: Entwickelte und wartete Webanwendungen mit React und Node.js. Implementierte das State-Management mit Redux Toolkit für skalierbare Anwendungen. Entwarf und optimierte MongoDB-Schemas für effiziente Datenspeicherung und -abfrage. Nutze Docker für die Containerisierung und Bereitstellung von Anwendungen. Erstellte RESTful APIs mit Express und dokumentierte diese mit Swagger. Führte Code-Reviews durch und pflegte Git-Repositories zur Versionskontrolle.',
+  brovko_4:
+    'Erfolge: Erfolgreich die Migration von Legacy-Code auf den modernen React- und Node.js-Stack geleitet, wodurch die Anwendungsleistung um 40 % verbessert wurde. Automatisierte Bereitstellungsprozesse mit Docker, wodurch die Bereitstellungszeit um 50 % verkürzt wurde. Aktive Teilnahme an Teammeetings und Beitrag zur Verbesserung der Entwicklungsabläufe und -praktiken.',
+  company: 'Anwaltskanzlei 911, Odessa, Ukraine',
+
+  company_2:
+    'Verantwortlichkeiten: Effektive Kommunikation mit Kunden, um deren Zufriedenheit sicherzustellen und Bedenken auszuräumen. Koordination, Durchführung und Planung von Aufgaben, um eine pünktliche Projektlieferung sicherzustellen. Überwachung und Leitung des Projektpersonals, um ein hohes Produktivitätsniveau aufrechtzuerhalten. Vorbereitung und Überprüfung von Rechtsdokumenten, um Konformität und Genauigkeit sicherzustellen. Pflege und Aktualisierung der Unternehmenswebsite, um aktuelle Informationen und Ressourcen bereitzustellen. Verwaltung von Budgets und Finanzaufsicht, um kosteneffizienten Betrieb sicherzustellen.',
+  company_3:
+    'Erfolge: Durch strategische Planung und effektives Management konnte der Umsatz des Unternehmens um 30 % gesteigert werden. Automatisierte Arbeitsprozesse in einer wichtigen Abteilung, was zu erheblichen Effizienzgewinnen und verbesserter Produktivität führte. Entwickelte und implementierte eine neue Methodik für die juristische Recherche, wodurch die Recherchezeit um 30 % reduziert und die Genauigkeit erhöht wurde.',
+  company_1: 'Anwaltskanzlei 911, Odessa, Ukraine',
+  company_4: 'Position: Rechtsanwalt Mai 2011 - Jan 2012',
+  company_5:
+    'Aufgaben: Bereitstellung von Rechtsberatung und Unterstützung von Klienten in einer Vielzahl von Rechtsangelegenheiten. Aufsetzen, Überprüfen und Aushandeln von Verträgen, Vereinbarungen und anderen Rechtsdokumenten. Vertretung von Klienten in Gerichtsverfahren, einschließlich Gerichts- und Schiedsverfahren. Zusammenarbeit mit anderen Rechtsexperten und externen Beratern zur Entwicklung von Fallstrategien.',
+  company_6:
+    'Erfolge: Entwicklung eines umfassenden Schulungsprogramms für junge Anwälte zur Verbesserung ihrer Fähigkeiten in der Formulierung von Rechtstexten und in der Verhandlungsführung.',
+  sopot: 'Sopot und Partner, Odessa, Ukraine',
+  sopot_1: 'Position: Rechtsanwalt September 2009 - April 2011',
+  sopot_2:
+    'Verantwortlichkeiten: Aufgaben: Erstellen Sie Rechtsgutachten, Anträge und Klageschriften für Gerichtsverfahren. Stellen Sie sicher, dass Rechtsdokumente rechtzeitig und genau vorbereitet und eingereicht werden. Beraten Sie zu Rechtsrisiken und unterstützen Sie bei der Entwicklung von Strategien zur Risikominderung.',
+  sopot_3:
+    'Erfolg: Beratung und Unterstützung bei der Lösung komplexer Rechtsstreitigkeiten, wodurch den Mandanten erhebliche potenzielle Prozesskosten erspart werden.',
+  projects: 'Arbeitsprojekte',
+  projects_1:
+    'Rolle: Full-Stack-Entwickler, Backend-Entwicklung: Benutzerverwaltung: Implementierung und Verwaltung benutzerbezogener Funktionen wie Benutzerregistrierung, Anmeldung und Profilverwaltung. Authentifizierung: Integrierte Google-Authentifizierungs- und Aktualisierungstoken-Mechanismen zur Verbesserung der Sicherheit und des Benutzererlebnisses. Refactoring: Wesentliches Refactoring von Backend-Komponenten wie Controllern, Routen, Middleware, Diensten und Modellen zur Verbesserung der Codequalität und Wartbarkeit durchgeführt. Dateiverwaltung: Nutzung von Multer für Datei-Uploads und Sharp zur Bildverarbeitung zur effektiven Verwaltung benutzerhochgeladener Inhalte. Frontend-Entwicklung: Benutzerfunktionen: Entwicklung benutzerzentrierter Funktionen wie Benutzerauthentifizierung, Warenkorbverwaltung und Bestellverlauf. Modale Fenster: Implementierung modaler Fenster zur Handhabung von Benutzerinteraktionen und Anzeige dynamischer Inhalte. Statusverwaltung: Refactoring von Redux-Store, -Diensten und -Hooks zur Optimierung der Statusverwaltung und Verbesserung der Anwendungsleistung.',
+  projects_2: 'Lernprojekte',
+  projects_3:
+    'Rolle: Full-Stack-Entwickler. Backend-Entwicklung: Entwicklung des Backends mit Node.js und Express zur Bearbeitung von Anfragen und Verwaltung von Daten. Erstellung von Routen und Controllern für Benachrichtigungen und Integration von Swagger für API-Dokumentation. Frontend-Entwicklung: Auf der Clientseite Verwendung von React und Redux Toolkit zur Erstellung dynamischer und reaktionsfähiger Benutzeroberflächen. Implementierung von Stilen und Komponenten für die Modale, um eine benutzerfreundliche Erfahrung sicherzustellen.',
+  projects_4:
+    'Rolle: Frontend-Entwicklung: Als Teil eines Teams auf die Entwicklung von Benutzeroberflächen konzentriert. Erstellung und Integration eines Ladeindikators und Benachrichtigungssystems zur Verbesserung der Benutzerinteraktion mit der Anwendung.',
+  projects_5:
+    'Rolle: Frontend-Entwicklung: Entwicklung eines Abschnitts mit einzigartiger Positionierung zur Verbesserung der optischen Attraktivität und des Benutzererlebnisses. Außerdem wurde ein modales Fenster erstellt, um eine bessere Benutzerinteraktion mit der Anwendung zu ermöglichen.',
+};
+
+export const translationsEN = {
+  contact: 'Contact Information',
+  land: 'Stuttgart, Germany',
+  technical: 'Technical Skills',
+  teamwork: 'Teamwork',
+  committed: 'Committed',
+  punctual: 'Punctual',
+  organizational: 'Organizational skills',
+  willingness: 'Willingness to learn',
+  languages: 'Languages',
+  deutsch: 'Deutsch - Upper Intermediate (B2)',
+  english: 'English - Intermediate (B1)',
+  ukrainian: 'Ukrainian - Native',
+  russian: 'Russian - Native',
+  education: 'Education',
+  school: 'IT School GoIT',
+  developer: 'Full-Stack Developer, Jun 2022 - May 2023',
+  university: 'National University „Odessa Maritime Academy“',
+  law: 'LLM, Maritime law, Aug 2003 - Jun 2009',
+  full: 'Full-Stack Developer',
+  summary: 'Summary',
+  text_first:
+    'I am a self-motivated Full-Stack Developer with a year of practical experience and a year of academic experience in programming. I have strong knowledge and skills in CSS, HTML, JavaScript, React, and Node.js. My coding journey includes the successful completion of one work project, as well as three team projects and two individual projects as part of my studies at the prestigious IT institute GoIT.',
+  text_second:
+    'I am currently looking for opportunities to work as a Front-End Developer with a focus on React or a Full-Stack Developer with an emphasis on JavaScript, React, and Node.js. I strive for continuous development, am a quick learner, and enjoy programming. I am actively working on my personal project, which allows me to constantly improve my skills and apply new knowledge in practice.',
+  text_third:
+    'I am seeking further development in the field of programming, with an emphasis on career growth, effective communication, and productive teamwork. My goal is to continually improve my skills and achieve high results through hard work and striving for excellence.',
+  experience: 'Work Experience',
+  brovko_1: 'Full-Stack Developer, Part-time: Aug 2023 – May 2024',
+  brovko_2:
+    'Technologies: HTML, Sass, JavaScript, Webpack, React, Redux Toolkit, Node.js, MongoDB, Express, Git, Docker, Swagger, Trello.',
+  brovko_3:
+    'Responsibilities: Developed and maintained web applications using React and Node.js. Implemented state management using Redux Toolkit for scalable applications. Designed and optimized MongoDB schemas for efficient data storage and query. Used Docker for containerization and deployment of applications. Built RESTful APIs using Express and documented them using Swagger. Performed code reviews and maintained Git repositories for version control.',
+  brovko_4:
+    'Achievement: Successfully led the migration of legacy code to modern React and Node.js stack, improving application performance by 40%. Automated deployment processes with Docker, decreasing deployment time by 50%. Actively participated in team meetings and contributed to the improvement of development workflows and practices.',
+  company: 'Law company 911, Odessa, Ukraine',
+  company_2:
+    'Responsibilities: Effectively communicated with clients to ensure satisfaction and address concerns. Coordinated, performed, and planned tasks to ensure timely project delivery. Supervised and managed project personnel to maintain high productivity levels. Prepared and reviewed legal documents to ensure compliance and accuracy. Maintained and updated the company website to provide current information and resources. Managed budgets and financial oversight to ensure cost-effective operations.',
+  company_3:
+    'Achievements: Influenced a 30% increase in company revenue through strategic planning and effective management. Automated work processes in a key department, resulting in significant efficiency gains and improved productivity. Developed and implemented a new methodology for legal research, reducing research time by 30% and increasing accuracy.',
+  company_1: 'Law company 911, Odessa, Ukraine',
+  company_4: 'Position: Lawyer May 2011 - Jan 2012',
+  company_5:
+    'Responsibilities: Provide legal advice and assist clients in a variety of legal matters. Draft, review, and negotiate contracts, agreements, and other legal documents. Represent clients in legal proceedings, including court appearances and arbitration. Collaborate with other legal professionals and outside counsel to develop case strategies.',
+  company_6:
+    'Achievement: Created a comprehensive training program for young lawyers that improved their skills in legal drafting and negotiation.',
+  sopot: 'Sopot and Partners, Odessa, Ukraine',
+  sopot_1: 'Position: Lawyer Sept 2009 - Apr 2011',
+  sopot_2:
+    'Responsibilities: Prepare legal briefs, motions and statements of claim for court proceedings. Ensure timely and accurate preparation and filing of legal documents. Advise on legal risks and assist in developing risk mitigation strategies.',
+  sopot_3:
+    'Achievement: Advising and assisting in resolving complex legal disputes, saving clients significant potential litigation costs.',
+  projects: 'Work Projects',
+  projects_1:
+    'Role: Full-stack developer, Backend Development: User Management: Implemented and managed user-related functionalities, including user registration, login, and profile management. Authentication: Integrated Google authentication and refresh token mechanisms to enhance security and user experience. Refactoring: Conducted significant refactoring of backend components including controllers, routes, middleware, services, and models to improve code quality and maintainability. File Handling: Utilized Multer for file uploads and Sharp for image processing to manage user-uploaded content effectively. Frontend Development: User Features: Developed user-centric features such as user authentication, cart management, and order history. Modal Windows: Implemented modal windows to handle user interactions and display dynamic content. State Management: Refactored Redux store, services, and hooks to optimize state management and improve application performance.',
+  projects_2: 'Learning Projects',
+  projects_3:
+    'Role: Full-stack developer. Backend Development: Developed the backend using Node.js and Express to handle requests and manage data. Created routes and controllers for notifications and integrated Swagger for API documentation. Frontend Development: On the client side, used React and Redux Toolkit to build dynamic and responsive user interfaces. Implemented styles and components for the modals, ensuring a user-friendly experience.',
+  projects_4:
+    'Role: Frontend Development: As part of a team, focused on developing user interfaces. Created and integrated a loading indicator and notification system to enhance user interaction with the application.',
+  projects_5:
+    'Role: Frontend Development: Developed a section with unique positioning to enhance visual appeal and user experience. Also created a modal window to facilitate better user interaction with the application.',
+};
---
+++ toggleLanguage.js
@@ -0,0 +1,27 @@
+import { translationsDE, translationsEN } from './index.js';
+
+const langButton = document.getElementById('languageButton');
+
+// langButton.innerText = 'DE';
+
+document.addEventListener('DOMContentLoaded', () => {
+  langButton.addEventListener('click', changeLanguage);
+});
+
+function changeLanguage() {
+  let keys = [];
+  let translations;
+
+  if (langButton.textContent === 'DE') {
+    translations = translationsDE;
+    langButton.innerText = 'EN';
+  } else {
+    translations = translationsEN;
+    langButton.innerText = 'DE';
+  }
+
+  keys = Object.keys(translations);
+  keys.forEach(key => {
+    document.getElementById(key).innerText = translations[key];
+  });
+}
